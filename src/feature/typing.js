3fc7056529e8df3109e510f61a5d2f7a24b658b9
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2023-06-13 00:15:32 -0300

    main: Start project. Pending alert message for error.

---
+++ src/feature/typing.js
@@ -0,0 +1,105 @@
+const text = document.querySelector("#text");
+const areaText = document.querySelector("#value");
+const restart = document.querySelector("#restart");
+const result = document.querySelector("#result");
+const historic = document.querySelector("#historic");
+const theme = document.querySelector("#theme");
+const alert = document.querySelector("#alert");
+const alertMessage = document.querySelector("#alert-message");
+const alertClose = document.querySelector("#alert-close");
+
+const texts = [
+  "Example text to type",
+  "Another example of text to type",
+  "Type this",
+  "You can type this here",
+];
+
+function newText() {
+  const index = Math.floor(Math.random() * texts.length);
+  areaText.focus();
+  text.textContent = texts[index];
+}
+
+function isProgressTest() {
+  return JSON.parse(localStorage.getItem("progress")) || false;
+}
+
+function setProgressTest(areaText) {
+  localStorage.setItem("progress", JSON.stringify(areaText));
+}
+
+function start() {
+  if (!isProgressTest()) {
+    localStorage.setItem("start", new Date().getTime());
+    setProgressTest(true);
+  }
+}
+
+function verify() {
+  const end = new Date().getTime();
+  const spent = (end - parseInt(localStorage.getItem("start"))) / 1000;
+  result.textContent = `Congratulations! You took ${spent.toFixed(2)} seconds.`;
+
+  addHistoric(text.textContent, spent);
+  setProgressTest(false);
+  areaText.value = "";
+  newText();
+}
+
+function addHistoric(typingText, end) {
+  const item = document.createElement("p");
+  item.textContent = `Text: "${typingText}" - Time: ${end} seconds`;
+  historic.appendChild(item);
+}
+
+function update() {
+  start();
+  const { value } = areaText;
+  if (value === text.textContent) {
+    verify();
+  }
+}
+
+function restartGame() {
+  areaText.value = "";
+  result.textContent = "";
+  newText();
+  setProgressTest(false);
+  historic.innerHTML = "";
+}
+
+function changeTheme() {
+  if (document.body.classList.contains("default")) {
+    document.body.classList.toggle("dark");
+    button = document.getElementById("theme");
+    button.innerHTML = "Dark";
+  }
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.toggle("default");
+    button = document.getElementById("theme");
+    button.innerHTML = "Light";
+  }
+}
+
+function close() {
+  alert.classList.add("hide");
+}
+
+// areaText.addEventListener("keydown", (event) => {
+//   if (event.keyCode === 13) {
+//     const { value } = areaText;
+//     if (!texts.includes(value)) {
+//       // alert.classList.remove("hide");
+//       // alertMessage.textContent = "You typed wrong, try again.";
+//     } else {
+//       update();
+//     }
+//   }
+// });
+// alertClose.addEventListener("click", () => close());
+areaText.addEventListener("keyup", () => update());
+restart.addEventListener("click", () => restartGame());
+theme.addEventListener("click", () => changeTheme());
+
+newText();
